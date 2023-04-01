da0232c9601ad70039030298ec4746e5d7053532
Shreshth Shukla <93610630+Shreshth-Shukla@users.noreply.github.com>
2023-04-02 01:34:07 +0530

    Add files via upload

---
+++ Notes App/Notes/Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Shreshth Shalini Shukla on 28/03/23.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
